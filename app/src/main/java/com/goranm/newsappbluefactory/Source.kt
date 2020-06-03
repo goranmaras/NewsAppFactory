01cb8de6c79569382450cfa65d395ddb8616c90c
Goran <goran.maras77@gmail.com>
2020-06-03 11:26:50 +0200

    added retrofit setup

---
+++ app/src/main/java/com/goranm/newsappbluefactory/Source.kt
@@ -0,0 +1,6 @@
+package com.goranm.newsappbluefactory
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/goranm/newsappbluefactory/util/Konstante.kt
@@ -0,0 +1,8 @@
+package com.goranm.newsappbluefactory.util
+
+class Konstante {
+    companion object{
+        const val API_KEY = "fc62c18d33fd4dea8cba94462d6dc6ad"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
