20edbe2334a33fbe211cdba9e05ecd4c14e68154
Goran <goran.maras77@gmail.com>
2020-06-04 10:42:32 +0200

    Handeld API Request and Response, BreakingNewsFragment.kt showing articles, added webview for signle item in list -> handeld with nav args data. Changed build.gradle files, added plugin for nav_graph

--- app/src/main/java/com/goranm/newsappbluefactory/repository/NewsRepository.kt
+++ app/src/main/java/com/goranm/newsappbluefactory/repository/NewsRepository.kt
@@ -1,8 +1,13 @@
 package com.goranm.newsappbluefactory.repository
 
+import com.goranm.newsappbluefactory.api.RetrofitInstance
 import com.goranm.newsappbluefactory.db.ArticleDatabase
 
 class NewsRepository(
     val db : ArticleDatabase
 ) {
+
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode,pageNumber)
+
 }
--- app/src/main/java/com/goranm/newsappbluefactory/ui/NewsViewModel.kt
+++ app/src/main/java/com/goranm/newsappbluefactory/ui/NewsViewModel.kt
@@ -1,9 +1,39 @@
 package com.goranm.newsappbluefactory.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.goranm.newsappbluefactory.model.NewsResponse
 import com.goranm.newsappbluefactory.repository.NewsRepository
+import com.goranm.newsappbluefactory.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository : NewsRepository
 ) : ViewModel() {
+
+    val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse>{
+        if (response.isSuccessful){
+            response.body()?.let {resultResponse->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
 }
